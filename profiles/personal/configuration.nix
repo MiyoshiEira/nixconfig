55dfa3c81839eab91a893a963e7dd634d7b2523e
三好エイラ <eira@miyoshi.app>
2024-06-16 19:01:46 +0200

    Integrated sops for future secret management

--- profiles/personal/configuration.nix
+++ profiles/personal/configuration.nix
@@ -31,6 +31,9 @@
 
     ];
 
+  sops.defaultSopsFile = ./secrets/secrets.yaml;
+  sops.defaultSopsFormat = "yaml";
+  sops.age.keyFile = "/home/miyoshieira/.config/sops/age/keys.txt";
 
   # Fix nix path
   nix.nixPath = [ "nixpkgs=/nix/var/nix/profiles/per-user/root/channels/nixos"
