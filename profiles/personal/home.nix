a4a7a1966a3d259ecdae762daf352128f4a26f7c
三好エイラ <eira@miyoshi.app>
2024-06-16 15:42:34 +0200

    test

--- profiles/personal/home.nix
+++ profiles/personal/home.nix
@@ -82,7 +82,23 @@
     
     
 ];
-
+  programs.zsh = {
+    enable = true;
+    autosuggestion.enable = true;
+    syntaxHighlighting.enable = true;
+    enableCompletion = true;
+    oh-my-zsh = {
+    enable = true;
+    theme = "robbyrussell";
+    plugins = [];
+    };
+    initExtra = ''
+    PROMPT=" ◉ %U%F{magenta}%n%f%u@%U%F{blue}%m%f%u:%F{yellow}%~%f
+     %F{green}→%f "
+    RPROMPT="%F{red}▂%f%F{yellow}▄%f%F{green}▆%f%F{cyan}█%f%F{blue}▆%f%F{magenta}▄%f%F{white}▂%f"
+    [ $TERM = "dumb" ] && unsetopt zle && PS1='$ '
+    '';
+  };
 
   #LunarVim config
   xdg.configFile."lvim/config.lua".source = ../../configs/lvim/config.lua;
