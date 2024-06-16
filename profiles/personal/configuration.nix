9e1aed420cb65d7fd00f8197e09f158dfb6066cf
三好エイラ <eira@miyoshi.app>
2024-06-16 19:09:45 +0200

    typo lmao

--- profiles/personal/configuration.nix
+++ profiles/personal/configuration.nix
@@ -31,7 +31,7 @@
 
     ];
 
-  sops.defaultSopsFile = ./secrets/secrets.yaml;
+  sops.defaultSopsFile = ../../secrets/secrets.yaml;
   sops.defaultSopsFormat = "yaml";
   sops.age.keyFile = "/home/miyoshieira/.config/sops/age/keys.txt";
   sops.secrets.testsecret = { };
