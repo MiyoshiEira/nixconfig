f849950a0b5c945edbbf50c9c6544d3decdc27c3
三好エイラ <eira@miyoshi.app>
2024-06-18 16:03:56 +0200

    Encrypted declarative settings

--- user/security/sops.nix
+++ user/security/sops.nix
@@ -12,7 +12,9 @@ sops
 sops = {
   defaultSopsFile = ../../secrets/secrets.yaml;
   age.keyFile = "/home/miyoshieira/.config/sops/age/keys.txt";
-  secrets."gitconf" = {};
+  secrets."gitconf" = {
+  path = "${config.home.homeDirectory}/.ssh/config";
+  };
   };
 }
 
