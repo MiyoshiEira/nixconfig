dce0a0c9e205c9e8a091ca7c054efa4288ba4de5
三好エイラ <eira@miyoshi.app>
2024-06-18 19:03:36 +0200

    add keys

--- user/security/sops.nix
+++ user/security/sops.nix
@@ -14,6 +14,19 @@ sops = {
   age.keyFile = "/home/miyoshieira/.config/sops/age/keys.txt";
   secrets."gitconf" = {
   path = "${config.home.homeDirectory}/.ssh/config";
+  mode = "0600";
+  };
+  secrets."nixgit" = {
+  path = "${config.home.homeDirectory}/.ssh/nixgit";
+  mode = "0600";
+  };
+  secrets."wkey" = {
+  path = "${config.home.homeDirectory}/.ssh/wkey";
+  mode = "0600";
+  };
+  secrets."wprod" = {
+  path = "${config.home.homeDirectory}/.ssh/wprod";
+  mode= "0600";
   };
   };
 }
