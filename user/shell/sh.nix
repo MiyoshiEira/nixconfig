1992de66355aed06ecc3b45c641b05a51f4ae3a2
三好エイラ <eira@miyoshi.app>
2024-06-19 16:27:59 +0200

    integrate pyenv to terminal

--- user/shell/sh.nix
+++ user/shell/sh.nix
@@ -28,6 +28,12 @@ in
     enableCompletion = true;
     shellAliases = myAliases;
     initExtra = ''
+
+    export PYENV_ROOT="$HOME/.pyenv"
+    [[ -d $PYENV_ROOT/bin ]] && export PATH="$PYENV_ROOT/bin:$PATH"
+    eval "$(pyenv init -)"
+
+
     if [ -z "$TMUX" ]
     then
        tmux attach -t TMUX || tmux new -s TMUX
