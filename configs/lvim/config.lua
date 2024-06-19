cb536127a854057730c0b8f311f121e85202b5ff
三好エイラ <eira@miyoshi.app>
2024-06-19 12:26:43 +0200

    better way

--- configs/lvim/config.lua
+++ configs/lvim/config.lua
@@ -11,10 +11,18 @@ lvim.plugins = {
   "nvim-neotest/nvim-nio",
   "nvim-neotest/neotest",
   "nvim-neotest/neotest-python",
-  "rcarriga/nvim-notify",
-  "MunifTanjim/nui.vnim",
-  "folke/noice.nvim",
   {
+{
+  "folke/noice.nvim",
+  event = "VeryLazy",
+  opts = {
+    -- add any options here
+  },
+  dependencies = {
+    "MunifTanjim/nui.nvim",
+    "rcarriga/nvim-notify",
+    },
+},
   "Pocco81/auto-save.nvim",
   config = function()
     require("auto-save").setup()
@@ -73,6 +81,26 @@ local mason_path = vim.fn.glob(vim.fn.stdpath "data" .. "/mason/")
 pcall(function()
   require("dap-python").setup(mason_path .. "packages/debugpy/venv/bin/python")
 end)
+-- noice config
+
+require("noice").setup({
+  lsp = {
+    -- override markdown rendering so that **cmp** and other plugins use **Treesitter**
+    override = {
+      ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+      ["vim.lsp.util.stylize_markdown"] = true,
+      ["cmp.entry.get_documentation"] = true, -- requires hrsh7th/nvim-cmp
+    },
+  },
+  -- you can enable a preset for easier configuration
+  presets = {
+    bottom_search = true, -- use a classic bottom cmdline for search
+    command_palette = true, -- position the cmdline and popupmenu together
+    long_message_to_split = true, -- long messages will be sent to a split
+    inc_rename = false, -- enables an input dialog for inc-rename.nvim
+    lsp_doc_border = false, -- add a border to hover docs and signature help
+  },
+})
 
 -- setup testing
 require("neotest").setup({
@@ -106,3 +134,4 @@ lvim.builtin.which_key.mappings["C"] = {
   name = "Python",
   c = { "<cmd>lua require('swenv.api').pick_venv()<cr>", "Choose Env" },
 }
+
