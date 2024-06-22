5fc591056a635c3b96b1689f57456a86f9d5c5d4
三好エイラ <eira@miyoshi.app>
2024-06-22 13:59:37 +0200

    configs

--- user/app/yazi/yazi.nix
+++ user/app/yazi/yazi.nix
@@ -3,17 +3,12 @@
 {
   programs.yazi = {
     enable = true;
-    enableZshIntegration = true;
-    flavors = {};
-    #initLua = {};
-    keymap = {};
-    plugins = {};
-    settings = {};
-    theme = {};
+    enableZshIntegration = true; 
 
   };
 
-#home.file."${config.home.homeDirectory}/.config/yazi/yazi.toml".source = ../../../configs/yazi/yazi.toml;
+home.file."${config.home.homeDirectory}/.config/yazi/yazi.toml".source = ../../../configs/yazi/yazi.toml;
+home.file."${config.home.homeDirectory}/.config/yazi/keymap.toml".source = ../../../configs/yazi/keymap.toml;
 
 }
 
