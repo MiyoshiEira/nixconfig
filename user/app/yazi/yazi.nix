327eeb64d78994c9ac72161e73ea8c59e9614d4f
三好エイラ <eira@miyoshi.app>
2024-06-22 14:00:37 +0200

    forgot one

--- user/app/yazi/yazi.nix
+++ user/app/yazi/yazi.nix
@@ -9,6 +9,6 @@
 
 home.file."${config.home.homeDirectory}/.config/yazi/yazi.toml".source = ../../../configs/yazi/yazi.toml;
 home.file."${config.home.homeDirectory}/.config/yazi/keymap.toml".source = ../../../configs/yazi/keymap.toml;
-
+home.file."${config.home.homeDirectory}/.config/yazi/theme.toml".source = ../../../configs/yazi/theme.toml;
 }
 
