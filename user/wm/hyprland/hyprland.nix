57a9ac7298719bec0bacce983ef9cee5881354ff
三好エイラ <eira@miyoshi.app>
2024-06-24 13:10:32 +0200

    swap to dwindle layout

--- user/wm/hyprland/hyprland.nix
+++ user/wm/hyprland/hyprland.nix
@@ -80,7 +80,7 @@ in
       }
 
       general {
-        layout = master
+        layout = dwindle
         border_size = 5
         col.active_border = 0xff'' + config.lib.stylix.colors.base08 + " " + ''0xff'' + config.lib.stylix.colors.base09 + " " + ''0xff'' + config.lib.stylix.colors.base0A + " " + ''0xff'' + config.lib.stylix.colors.base0B + " " + ''0xff'' + config.lib.stylix.colors.base0C + " " + ''0xff'' + config.lib.stylix.colors.base0D + " " + ''0xff'' + config.lib.stylix.colors.base0E + " " + ''0xff'' + config.lib.stylix.colors.base0F + " " + ''270deg
 
