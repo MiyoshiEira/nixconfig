f7b007f3dbbcc7b132b3e40d372887229fb75d61
三好エイラ <eira@miyoshi.app>
2024-06-29 14:31:33 +0200

    fix fonts

--- system/wm/fonts.nix
+++ system/wm/fonts.nix
@@ -2,13 +2,18 @@
 
 {
   # Fonts are nice to have
-  fonts.packages = with pkgs; [
+  fonts = {
+  packages = with pkgs; [
     # Fonts
-    noto-fonts-cjk
-    noto-fonts-cjk-serif
-    noto-fonts-cjk-sans
-    noto-fonts-emoji
-    nerdfonts
-  ];
+    noto-fonts-cjk noto-fonts-cjk-serif noto-fonts-cjk-sans noto-fonts-emoji nerdfonts ];
+  fontconfig = {
+    defaultFonts = {
+      serif = [ "Noto Fonts CJK Serif" ];
+      sansSerif = [ "Noto Fonts CJK Sans" ];
+      monospace = [ "Noto Fonts CJK" ];
+  };
 
+  };
+
+};
 }
