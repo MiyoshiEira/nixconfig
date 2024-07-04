773691253ff83c3ece906b8e92ab80e5eb9264c8
MiyoshiEira <eira@miyoshi.app>
2024-07-04 10:26:59 +0200

    Config additions

--- configs/doom.d/config.el
+++ configs/doom.d/config.el
@@ -74,3 +74,74 @@
 ;;
 ;; You can also try 'gd' (or 'C-c c d') to jump to their definition and see how
 ;; they are implemented.
+
+
+;; Custom Settings
+;; Window management binds
+(bind-key* "C-j" #'evil-window-down)
+(bind-key* "C-k" #'evil-window-up)
+(bind-key* "C-h" #'evil-window-left)
+(bind-key* "C-l" #'evil-window-right)
+(bind-key* "C-q" #'evil-window-delete)
+
+;;Buffer management
+(bind-key* "<mouse-9>" #'next-buffer)
+(bind-key* "<mouse-8>" #'previous-buffer)
+
+;; GC
+(add-hook 'after-init-hook
+          #'(lambda ()
+              (setq gc-cons-threshold (* 100 1024 1024))))
+(add-hook 'focus-out-hook 'garbage-collect)
+(run-with-idle-timer 5 t 'garbage-collect)
+
+;; Autoupdate buffer on filechange
+(setq global-auto-revert-mode nil)
+(setq auto-revert-use-notify t)
+
+;; Org mode configuration
+(setq org-directory "~/.Org")
+
+(set-company-backend! 'org-mode nil)
+
+(setq org-startup-with-inline-images t
+      org-image-actual-width nil)
+
+;; Frame borders and dividers
+(modify-all-frames-parameters
+ '((right-divider-width . 5)
+   (internal-border-width . 5)))
+(dolist (face '(window-divider
+                window-divider-first-pixel
+                window-divider-last-pixel))
+  (face-spec-reset-face face)
+  (set-face-foreground face (face-attribute 'default :background)))
+(set-face-background 'fringe (face-attribute 'default :background))
+
+(setq
+ org-auto-align-tags nil
+ org-tags-column 0
+ org-catch-invisible-edits 'show-and-error
+ org-special-ctrl-a/e t
+ org-insert-heading-respect-content t
+ org-hide-emphasis-markers t
+ org-pretty-entities t
+ org-ellipsis "...")
+
+(setq-default line-spacing 0)
+
+;; Tangle on save
+(defun tangle-on-save-org-mode-file()
+  (when (string= (message "%s" major-mode) "org-mode")
+    (org-babel-tangle)))
+(add-hook 'after-save-hook 'tangle-on-save-org-mode-file)
+
+;; Org Source blocks styling
+(setq electric-indent-mode nil)
+(setq org-src-window-setup 'current-window)
+(set-popup-rule! "^\\*Org Src"
+  :side 'top'
+  :size 0.9)
+
+;; Org Roam Config
+(require 'org-roam)
