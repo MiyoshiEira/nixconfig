0da17675351f6b819fd5a947d8335c4c3da0c9cb
三好エイラ <eira@miyoshi.app>
2024-07-15 16:24:54 +0200

    wezterm styling

--- modules/home/required/shell/wezterm.nix
+++ modules/home/required/shell/wezterm.nix
@@ -10,8 +10,11 @@
   package = inputs.wezterm.packages.${systemSettings.system}.default;
   extraConfig = ''
   return {
-  color_scheme = "Catppuccin Mocha"
-  }
+  color_scheme = "Catppuccin Mocha",
+  font = wezterm.font("CaskaydiaCove Nerd Font"),
+  font_size = 18.0,
+  window_background_opacity = 0.8
+}
 '';
   };
 }
