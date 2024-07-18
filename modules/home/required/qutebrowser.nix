20c3faf84cc082baa9f40c2ba1464818234e23f7
MiyoshiEira <eira@miyoshi.app>
2024-07-18 09:20:15 +0200

    qutebrowser initial config

--- modules/home/required/qutebrowser.nix
+++ modules/home/required/qutebrowser.nix
@@ -2,4 +2,5 @@
 
 {
   home.packages = with pkgs; [qutebrowser];
+  xdg.configFile."/.config/qutebrowser/config.py".source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/.dotfiles/configs/qutebrowser/config.py";
 }
