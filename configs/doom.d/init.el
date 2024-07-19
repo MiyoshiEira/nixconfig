5ff214ab9cd97fa824c8dcf1c5c9176e40571890
三好エイラ <eira@miyoshi.app>
2024-07-19 07:24:25 +0200

    add some LSPs

--- configs/doom.d/init.el
+++ configs/doom.d/init.el
@@ -139,24 +139,24 @@
        haskell    ; a language that's lazier than I am
        ;;hy                ; readability of scheme w/ speed of python
        ;;idris             ; a language you can depend on
-       json              ; At least it ain't XML
-       java       ; the poster child for carpal tunnel syndrome
+       (json +lsp)              ; At least it ain't XML
+       (java +lsp)       ; the poster child for carpal tunnel syndrome
        (javascript +lsp)        ; all(hope(abandon(ye(who(enter(here))))))
        ;;julia             ; a better, faster MATLAB
        ;;kotlin            ; a better, slicker Java(Script)
        latex             ; writing papers in Emacs has never been so fun
        ;;lean              ; for folks with too much to prove
        ;;ledger            ; be audit you can be
-       lua               ; one-based indices? one-based indices
+       (lua +lsp)               ; one-based indices? one-based indices
        markdown          ; writing docs for people to ignore
        ;;nim               ; python + lisp at the speed of c
-       nix               ; I hereby declare "nix geht mehr!"
+       (nix +lsp)               ; I hereby declare "nix geht mehr!"
        ;;ocaml             ; an objective camel
-       (org +roam2 +dragndrop +pretty +hugo)               ; organize your plain life in plain text
+       (org +roam2 +dragndrop +pretty +hugo +journal +pomodoro )               ; organize your plain life in plain text
        php               ; perl's insecure younger brother
        ;;plantuml          ; diagrams for confusing people more
        ;;purescript        ; javascript, but functional
-       (python +lsp +tree-sitter +conda)            ; beautiful is better than ugly
+       (python +lsp +tree-sitter +conda +pyright)            ; beautiful is better than ugly
        qt                ; the 'cutest' gui framework ever
        ;;racket            ; a DSL for DSLs
        ;;raku              ; the artist formerly known as perl6
