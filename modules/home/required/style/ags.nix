b1489977c23ef10d50daa80c7b35de9e37d6ce99
三好エイラ <eira@miyoshi.app>
2024-07-20 08:43:35 +0200

    move initial ags conf

--- modules/home/required/style/ags.nix
+++ modules/home/required/style/ags.nix
@@ -3,8 +3,15 @@
 {
 imports = [ inputs.ags.homeManagerModules.default  ];
 
+home.packages = with pkgs; [
+  bun
+  dart-sass
+  matugen
+
+];
 programs.ags = {
   enable = true;
+  configDir = ../../../../configs/ags;
 
 };
 }
